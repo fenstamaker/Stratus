3f7db8a4e1f3827884e3791ce34c89bd75a6865f
Gary Fenstamaker <fenstamaker@gmail.com>
2014-08-14 13:27:56 -0400

    Added dist folder

---
+++ dist/Stratus.js
@@ -0,0 +1,22 @@
+/*! Stratus 2014-08-13 */
+requirejs.config({baseUrl:"src",paths:{react:"../bower_components/react/react.min.js",jsx:"../bower_components/react/JSXTransformer.js",immutable:"../node_modules/immutable/dist/Immutable.js"}}),requirejs(["react","immutable"],function(){});
+/*
+ * Stratus Init App
+ */
+'use strict';
+
+(function(Paragraph) {
+    return function() {
+        var app = app || {};
+
+        var Stratus = {
+            init: function() {
+
+            },
+            render: function() {
+
+            }
+        };
+    };
+})(Paragraph);
+
